13b7b879c1dac1567a814fd06924992343dd5582
Madhushaa <madhushaa123@gmail.com>
2025-04-11 17:04:45 +0800

    learn database intro

--- moodify_1-11.py
+++ moodify_1-11.py
@@ -1,3 +1,28 @@
 import tkinter
-import pygame 
+from tkinter import*
+from PIL import ImageTk, Image
+import pygame
+import sqlite3
 
+root = Tk()
+root.title('database')
+root.geometry("400x400")
+
+#Create database or connect to existing one
+conc = sqlite3.connect('users.db')
+
+#Create cursor
+cur = conc.cursor()
+
+#Create table
+cur.execute("""CREATE TABLE username (
+            username text, 
+            gender text,
+            )""")
+
+
+#Update changes
+conc.commit()
+#Close conection
+conc.close()
+root.mainloop()
