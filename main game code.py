6bf9f3de5bfadc3e3e7e4f2052853f7e90f8a770
Coshin Lee <Coshinlee@gmail.com>
2025-04-13 12:32:11 +0800

    Changed the fullscreen exit problem with the ESC button

--- main game code.py
+++ main game code.py
@@ -8,9 +8,10 @@ screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 #game main loop
 while True:
     for event in pygame.event.get(): #collects all the events and goes through it one by one
-        if event.type ==pygame.QUIT: #check one by one if there is a quit event
-            pygame.quit()
-            exit()
+        if event.type == pygame.KEYDOWN:#check if any key is press 
+            if event.key == pygame.K_ESCAPE: #if its the ESC key
+                pygame.quit() # shut down eveythig u open/ initialized (includes the program that is running in the background)
+                exit() 
 
     pygame.display.update() 
     #update the display of the screen on top
