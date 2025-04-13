624586da062cf98cbd52f841ed659462ff31dd9e
Coshin Lee <Coshinlee@gmail.com>
2025-04-13 23:03:53 +0800

    background added, frame rate set to 60 fps, esc button done

--- main game code.py
+++ main game code.py
@@ -1,9 +1,18 @@
 import pygame
 from sys import exit
 
-pygame.init()
+pygame.init() # to start the system: sound,graphics etc of pygame module
+#screen here is like a canvas to store the window and u can draw/ add other images
 screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-# set default full screen set the dimentsion and flag
+# tells pygame automatically set to full-screen on users computer 
+Time = pygame.time.Clock()
+#set game speed
+
+#background image
+background_surface =pygame.image.load("Moodify/graphics/Game main page.png")
+# to scale background image to match full screen of user
+screen_width,screen_height = screen.get_size()
+background_surface = pygame.transform.scale(background_surface,(screen_width,screen_height))
 
 #game main loop
 while True:
@@ -12,6 +21,8 @@ while True:
             if event.key == pygame.K_ESCAPE: #if its the ESC key
                 pygame.quit() # shut down eveythig u open/ initialized (includes the program that is running in the background)
                 exit() 
+    
+    screen.blit(background_surface,(0,0))
 
-    pygame.display.update() 
-    #update the display of the screen on top
+    pygame.display.update() #update the display of the screen 
+    Time.tick(60)# tells loop dont just faster then 60 fps
