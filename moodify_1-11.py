717df35bc71a06ff6be6b583c68437df71be6efc
Madhushaa <madhushaa123@gmail.com>
2025-04-16 20:52:51 +0800

    can save data inputted to database

--- moodify_1-11.py
+++ moodify_1-11.py
@@ -2,12 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 import sqlite3
 
-#Connect to database
-connect = sqlite3.connect('user_info.db')
-    
-#Create cursor
-cursor = connect.cursor()
-
 # Create the Tkinter root window
 root = tk.Tk()
 root.title("database")
@@ -25,45 +19,52 @@ center_frame.place(relx=0.5, rely=0.5, anchor='center')
 #Frames for UI
 moodify_frame = tk.LabelFrame(center_frame, text="Moodify", font=("Helvetica", 18, "bold"),bg="#fdf6f0") #fg=foreground/text color
 moodify_frame.grid(row=0, column=0, padx=50, pady=50)
-username_label = tk.Label(moodify_frame, text="Username:", bg="#e6e0d8")
-username_label.grid(row=0, column=0)
+profile_label = tk.Label(moodify_frame, text="Profile:", bg="#e6e0d8")
+profile_label.grid(row=0, column=0)
 gender_label = tk.Label(moodify_frame, text="Gender:", bg="#e6e0d8")
 gender_combobox = ttk.Combobox(moodify_frame, values=["Male", "Female"])
 gender_label.grid(row=3, column=0)
                                
  #User entry box
-username_entry = tk.Entry(moodify_frame)     
-username_entry.grid(row=2, column=0)     
+profile_entry = tk.Entry(moodify_frame)     
+profile_entry.grid(row=2, column=0)     
 gender_combobox.grid(row=4, column=0)     
 
 #Padding for both elements  
 for widget in moodify_frame.winfo_children():
         widget.grid_configure(padx=30, pady=30)
- 
- #Save data when button is clicked
+
+#Save data when button is clicked
 def enter_data(): 
         #Get user info
-        username = username_entry.get()
+        profile = profile_entry.get()
         gender = gender_combobox.get()
         #Display received data
-        print(f"Username: {username}, Gender: {gender}") 
+        print(f": {profile}, Gender: {gender}") 
+        #Connect to database
+        connect = sqlite3.connect('user_info.db')
+        #Create cursor
+        cursor = connect.cursor()
+        
+        #Create table
+        table_create_query = """ CREATE TABLE IF NOT EXISTS user_info
+                (profile TEXT, gender TEXT) """
+        cursor.execute(table_create_query)
+
+        #Insert Data
+        data_insert_query = """ INSERT INTO user_info
+        (profile, gender) VALUES
+        (?, ?)"""
+        data_insert_tuple = (profile, gender)
+        connect.execute(data_insert_query, data_insert_tuple)
+
+        #Save data, update
+        connect.commit()
+        #Close connection
+        connect.close()
     
 #Submit button   
 button = tk.Button(moodify_frame, text="SUBMIT", command= enter_data)  
 button.grid(row=7, column=0, sticky="news", padx=30, pady=30)                          
-
-#----------------------------------------------------------------------UI-----------------------------------------------------------------------------------
-       
-#Create table
-table_create_query = ''' CREATE TABLE IF NOT EXISTS user_data 
-        (username TEXT, gender TEXT)
-'''
-connect.execute(table_create_query)
-
-#Update changes
-connect.commit()
-
-#Close connection
-connect.close()
 
 root.mainloop()
