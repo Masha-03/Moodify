1b9c4847a0a7f3b6e5f0590c5157aa8ac9bd29df
Madhushaa <madhushaa123@gmail.com>
2025-04-30 14:16:46 +0800

    make sure there's entry before saving

--- calendar_.py
+++ calendar_.py
@@ -86,17 +86,25 @@ root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")  #Full-
 root.title("Calendar📅")
 root.configure(bg="#FCF8E8") #change the background color of entire window
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
+
 #Title label
 title=tk.Label(root, text="My Calendar🧸", font=("Helvetica", 18, "bold"),bg="#FCF8E8",fg="#333") #fg=foreground/text color
 title.pack(pady=10) #pack()=Places the widget inside the window or frame. Pady=Adds () pixels of vertical space around the widget.
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
 
 #frame for left side #create this frame is because pack and grid cannot use at the same time,need to seperate them
 left_frame=tk.Frame(root, bg="#FCF8E8")
 left_frame.pack(side="left", fill="y", padx=20, pady=20) #pack=geometry manager #padx=add horizontal padding #pady=add vertical padding
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
+
 #add a label for the title
 label=tk.Label(left_frame,text="Choose on a date to view your diary",font=("Helvetica",11),bg="#FCF8E8",fg="#777")
 label.grid(row=0, column=0, padx=10, pady=(5, 10), sticky="w") #sticky="w" means stick to the west
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
 
 #create a calendar widget with themed colors
 calendar=Calendar(left_frame, selectmode="day", date_pattern="yyyy-mm-dd", 
@@ -110,19 +118,29 @@ calendar=Calendar(left_frame, selectmode="day", date_pattern="yyyy-mm-dd",
 #the frame around the calendar
 calendar.grid(row=1, column=0, padx=10, pady=10) 
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
+
 #choose date button
 choosedate_btn = tk.Button(left_frame,text="Choose Date",font=("Arial Rounded MT Bold",18),bg="#F7E7CE"
                     ,bd=3,relief="groove",activebackground="#F99982", #activebackground=background while pressed
                     activeforeground="black",command=grab_date) #activeforeground=text color on click
 choosedate_btn.grid(row=2, column=0, pady=10)
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
+
 #the display of the date yyyy-mm-dd
 date_label =tk.Label(left_frame,text="",font=("Arial Rounded MT Bold",15),bg="#FCF8E8") #display
 date_label.grid(row=3, column=0, pady=(10, 0), sticky="n")
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
+
 #History frame
 history_frame = tk.Frame(root, bg="#FFF0D9", bd=2, relief="ridge")
-history_frame.pack(expand=True, fill="both", padx=60, pady=60)
+history_frame.pack(expand=True, fill="both", padx=40,pady=40)
+
+#diary's title history
+history_title=tk.Label(history_frame, text="History", font=("Comic Sans MS",13,"bold"),bg="#FFF0D9")
+history_title.pack()
 
 #Title label and value
 title_label = tk.Label(history_frame, text="Title:", font=("Arial", 12, "bold"), bg="#FFF0D9", fg="#444")
@@ -149,6 +167,8 @@ mooddesc_label.pack(anchor="w", padx=20, pady=(0, 5))
 
 mooddesc_display = tk.Label(history_frame, text="", font=("Arial", 11), bg="white", fg="#333", bd=1, relief="groove", padx=10, pady=5, anchor="center", justify="left", wraplength=500)
 mooddesc_display.pack(fill="x", padx=20, pady=(0, 10))
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
 
 #run the whole program
 root.mainloop()
